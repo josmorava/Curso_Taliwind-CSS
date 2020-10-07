94685f1b12772a3f658fb2abc03acfa9db471b4e
josmorava <josemorales-1234@hotmail.es>
2020-10-07 08:30:32 -0400

    Dimensiones y Espacios

--- platzifood/tailwind.config.full.js
+++ platzifood/tailwind.config.full.js
@@ -139,7 +139,7 @@ module.exports = {
       '1': '0.25rem',
       '2': '0.5rem',
       '3': '0.75rem',
-      '4': '1rem',
+      '4': '1rem', //equivale a 16 pixeles
       '5': '1.25rem',
       '6': '1.5rem',
       '8': '2rem',
